f19e6fe02308c0109b6a43f0c404b19b0d55de4a
TyroneMartin <tyrone306@byui.edu>
2024-05-01 08:20:32 -0600

    update

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,6 @@ const baseController = require('../controllers');
 routes.get('/', baseController.getName);
 
 
+
+
 module.exports = routes;
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 
 app.use('/', require('./routes'))
+// app.use('/contacts', require('./contacts'))
+
 
 const port = process.env.PORT || 3000;
 
