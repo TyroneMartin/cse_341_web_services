58a94d684fb45d9e5e84088a0985ddbb9cceab74
TyroneMartin <tyrone306@byui.edu>
2024-05-05 15:47:53 -0600

    update

--- routes/contacts.js
+++ routes/contacts.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const contactsController = require('../controllers/contacts');
 
 
-router.get('/', contactsController.getAll);   // http://localhost:3000/contacts
-router.get('/:id', contactsController.getSingle); // http://localhost:3000/contacts/_id
+router.get('/', contactsController.getAll);   // http://localhost:8080/contacts
+router.get('/:id', contactsController.getSingle); // http://localhost:8080/contacts/_id
 
 module.exports = router;
 
