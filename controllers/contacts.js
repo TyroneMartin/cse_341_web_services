7f8162a685edd316b328725f326b357327ec7691
TyroneMartin <tyrone306@byui.edu>
2024-05-05 19:31:28 -0600

    CRUD routes was included to create, read, update and delete the contacts list for the json document

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,5 +1,6 @@
 const mongodb = require('../db/connect');
-const ObjectId = require('mongodb').ObjectId;
+const {ObjectId} = require('mongodb'); // or ObjectID 
+// const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res, next) => {
   const result = await mongodb.getDb().db().collection('contacts').find();
@@ -30,7 +31,8 @@ const getSingle = async (req, res, next) => {
     .getDb()
     .db()
     .collection('contacts')
-    .findOne({ _id: userId });
+    // .find({ _id: userId });
+    .findOne({ _id: userId });  // find one is used to get a single document/contact
 
   if (result) {
     res.setHeader('Content-Type', 'application/json');
@@ -40,4 +42,69 @@ const getSingle = async (req, res, next) => {
   }
 };
 
-module.exports = { getAll, getSingle };
+
+const createContact = async (req, res) => {
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const response = await mongodb
+  .getDb()
+  .db()
+  .collection('contacts')
+  .insertOne(contact); // insertOne is used to insert a single document/contact
+  if (response.acknowledged) {  // If the insertion is successful/true 
+    res.status(201).json(response); // 201 is created status code for successful insertion
+  } else { 
+    res.status(500).json(response.error || 'Some error occurred while creating the contact.'); // 500 is internal server error
+  }
+};
+
+const updateContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  // be aware of updateOne if you only want to update specific fields
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const response = await mongodb
+    .getDb()
+    .db()
+    .collection('contacts')
+    .replaceOne({ _id: userId }, contact); // replaceOne is used to update a single document
+  console.log(response);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+  }
+};
+
+const deleteContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const response = await mongodb
+  .getDb()
+  .db()
+  .collection('contacts')
+  .remove({ _id: userId }, true); // remove is used to delete a single document
+  console.log(response);
+  if (response.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
+  }
+};
+
+module.exports = {
+  getAll,
+  getSingle,
+  createContact,
+  updateContact,
+  deleteContact
+};
--- routes/contacts.js
+++ routes/contacts.js
@@ -6,6 +6,10 @@ const contactsController = require('../controllers/contacts');
 router.get('/', contactsController.getAll);   // http://localhost:8080/contacts
 router.get('/:id', contactsController.getSingle); // http://localhost:8080/contacts/_id
 
+router.post('/', contactsController.createContact);
+router.put('/:id', contactsController.updateContact);
+router.delete('/:id', contactsController.deleteContact);
+
 module.exports = router;
 
 
