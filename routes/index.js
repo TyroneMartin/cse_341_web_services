cff041f52cba38a1eac469eef3a78de111d38604
TyroneMartin <tyrone306@byui.edu>
2024-05-15 00:43:59 -0600

    The Swagger framework was added to allow developer/s to create interactive, machine and human-readable API documentation--set of rules, specifications and tools for developing and describing RESTful APIs

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,17 @@ const express = require('express');
 const router = express.Router();
 const baseController = require('../controllers');
 
+
+const swaggerUi = require('swagger-ui-express');
+// const swaggerDocument = require('../swagger-output.json');
+const swaggerDocument = require('../swagger.json');
+
+
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
+
+
 router.get('/', baseController.getName);
 
 router.use('/contacts', require('./contacts'))
---
+++ swagger-generate.js
@@ -0,0 +1,14 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'My API',
+    description: 'Description'
+  },
+  host: 'localhost:8080'
+};
+
+const outputFile = './swagger-output.json';
+const routes = ['./server.js'];
+
+swaggerAutogen(outputFile, routes, doc);
