13c85e7d326a96efda6eed1b27a72b0b5a3d1ffe
TyroneMartin <tyrone306@byui.edu>
2024-05-17 14:19:11 -0600

    update

--- swagger-generate.js
+++ swagger-generate.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'My API',
     description: 'Description'
   },
-  host: ['localhost:8080', 'https://cse-341-web-services-imke.onrender.com']
+  host: ['localhost:8080', 'https://cse-341-web-services-imke.onrender.com/']
 };
 
 // const outputFile = './swagger-output.json';
